42969b6dda86a99a56e61bdfbc1b3755586f8aa9
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-06-23 19:49:37 +0530

    Update GetApiData.jsx

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GetApiData = async() => {
     const apiKey=import.meta.env.VITE_MOVIE_API_KEY;
     try{
-        const response=await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=avengers&page=1`);
+        const response=await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=avengers&page=1`);
         const data=response.json();
         return data;
     }catch(error){
